268110f393617cadc4729ce72c1356dbaf55e032
Todor Kostov <todor.kostov@bianor.com>
2016-03-27 18:47:55 +0300

    Initial commit

---
+++ app/src/main/java/simple/framework/activities/HomeActivity.java
@@ -0,0 +1,39 @@
+package simple.framework.activities;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.simple.framework.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    public static final String TAG = HomeActivity.class.getSimpleName();
+
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home_activity);
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+}
+
